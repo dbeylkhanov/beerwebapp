f1d5451a508ac284285e49a4f1d2fa6b51e01e80
Damir Beylkhanov <damir.beylkhanov@muranosoft.com>
2018-05-22 13:35:17 +0300

    extracted all beer action methods to the new beercontroller

---
+++ BeerWebApp/Controllers/BeerController.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using BeerApp.Bll.Beers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerApp.Web.Controllers
+{
+	[Route("api")]
+	public class BeerController : Controller
+	{
+		private readonly IBeerManager _beerManager;
+
+		public BeerController(IBeerManager beerManager)
+		{
+			_beerManager = beerManager;
+		}
+
+		[HttpGet]
+		[Route("[action]")]
+		public async Task<IActionResult> Beers(string query)
+		{
+			var result = await _beerManager.GetBeers(query);
+
+			return Ok(result);
+		}
+
+		[HttpGet]
+		[Route("[action]/{id}")]
+		public async Task<IActionResult> Beer([Required]string id)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = await _beerManager.GetBeerById(id);
+
+				return Ok(result);
+			}
+
+			return BadRequest("Beer Id is required");
+		}
+	}
+}
--- BeerWebApp/Controllers/HomeController.cs
+++ BeerWebApp/Controllers/HomeController.cs
@@ -5,37 +5,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BeerApp.Web.Controllers
 {
-	[Route("api/Home")]
     public class HomeController : Controller
     {
-	    private readonly IBeerManager _beerManager;
-
-	    public HomeController(IBeerManager beerManager)
+	   
+	    
+	    public IActionResult Index()
 	    {
-		    _beerManager = beerManager;
+		    return View();
 	    }
 
-		[HttpGet]
-		[Route("Beers")]
-        public async Task<IActionResult> GetAllBeers()
-		{
-			var result = await _beerManager.GetAllBeers();
-
-			return Ok(result);
-		}
-
-	    [HttpGet]
-	    [Route("Beer/{id}")]
-	    public async Task<IActionResult> GetBeerById([Required]string id)
-	    {
-		    if (ModelState.IsValid)
-		    {
-			    var result = await _beerManager.GetBeerById(id);
-
-			    return Ok(result);
-		    }
-
-		    return BadRequest("Beer Id is required");
-	    }
+	
     }
 }
